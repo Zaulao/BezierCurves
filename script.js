5370ca32124c63bd13f4ed2b29b843ed2e641b75
Zaulao <sgfl@cin.ufpe.br>
2018-12-18 11:30:50 -0300

    ajuste no tamanho do canvas e teste de click n' draw

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var container = document.getElementById('container');
+var canvas = document.getElementById('canvas');
+
 function resizeCanvas(width, height) {
     canvas.width = width;
     canvas.height = height;
@@ -7,62 +10,21 @@ function resizeToFit() {
     var height = parseFloat(window.getComputedStyle(canvas).height);
     resizeCanvas(width, height);
 }
+resizeToFit();
 
-function isInCircle(click) {
-    var v = {
-        x: circle.x - click.x,
-        y: circle.y - click.y
-    };
-    return (Math.sqrt(v.x * v.x + v.y * v.y) <= circle.radius);
-}
+canvas.addEventListener("click", draw, false);
 
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // ctx.beginPath();
-    // ctx.lineWidth = '5';
-    // ctx.fillStyle = '#BE9063';
-    // ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
-    // ctx.stroke();
-    // ctx.fill();
-
+function draw(e) {
+    var rect = canvas.getBoundingClientRect();
+    var ctx = canvas.getContext('2d');
+    posx = e.pageX-rect.left;
+    posy = e.pageY-rect.top;
     ctx.beginPath();
     ctx.fillStyle = "#BE9063";
-    ctx.arc(circle.x, circle.y, circle.radius/2 , 0, 2*Math.PI);
+    ctx.arc(posx, posy, 8 , 0, 2*Math.PI);
     ctx.fill();
     ctx.beginPath();
     ctx.strokeStyle = "#BE9063";
-    ctx.arc(circle.x, circle.y, circle.radius , 0, 2*Math.PI);
+    ctx.arc(posx, posy, 16 , 0, 2*Math.PI);
     ctx.stroke();
 }
-
-var container = document.getElementById('container');
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
-var circle = {
-    x: 100,
-    y: 100,
-    radius: 16
-};
-var move = false;
-
-resizeToFit();
-draw();
-
-canvas.addEventListener('mousedown', function(e) {
-    move = isInCircle({
-        x: e.offsetX,
-        y: e.offsetY
-    });
-});
-
-canvas.addEventListener('mousemove', function(e) {
-    if (move) {
-        circle.x = e.offsetX;
-        circle.y = e.offsetY;
-        draw();
-    }
-});
-
-canvas.addEventListener('mouseup', function(e) {
-    move = false;
-});
