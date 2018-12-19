079a622226690d61b4039d4f53e0cbb80384c9ca
Zaulao <sgfl@cin.ufpe.br>
2018-12-19 14:03:14 -0300

    checkboxes working

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var degree = document.getElementById('degree');
 var eval = document.getElementById('evaluations');
 var newDegree = document.getElementById('degree').value;
 var newEval = document.getElementById('evaluations').value;
+var controlPointsElem = document.getElementById('controlPoints');
+var controlPoligonalElem = document.getElementById('controlPoligonal');
+var bezierCurveElem = document.getElementById('BezierCurve');
 
 var ctx = canvas.getContext('2d');
 var points = [];
@@ -11,12 +14,89 @@ var curves = [];
 var count = +newDegree+1;
 canvas.width = window.innerWidth;
 
+controlPointsElem.addEventListener(("change"), (e) => {
+    drawEverything();
+});
+controlPoligonalElem.addEventListener(("change"), (e) => {
+    drawEverything();
+});
+bezierCurveElem.addEventListener(("change"), (e) => {
+    drawEverything();
+});
+console.log(controlPointsElem.checked)
+
+function drawEverything() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if(controlPointsElem.checked){
+        for (var i=0;i<curves.length;i++){
+            for(var j=0;j<curves[i].length; j++){
+                drawPoints(curves[i][j]);
+            }
+        }
+    }
+
+    if(controlPoligonalElem.checked){
+        for(var i=0;i<curves.length;i++){
+            drawPolygonal(i)
+        }
+    }
+
+    if(bezierCurveElem.checked){
+        for(var i=0;i<curves.length;i++){
+            drawCurve(ctx, curves[i], newEval)
+        }
+    }
+
+}
+
+
 class Point {
     constructor(x,y){
         this.x = x;
         this.y = y;
     }
 }
+
+function isInCircle(circle, click) {
+    var v = {
+        x: circle.x - click.x,
+        y: circle.y - click.y
+    };
+    return (Math.sqrt(v.x * v.x + v.y * v.y) <= circle.radius);
+}
+
+var moveIndexI = 0;
+var moveIndexJ = 0;
+
+function clickedAnyPoint(click){
+    var touchedAny = false;
+
+    for (let i = 0; i < curves.length; i++) {
+        for(let j=0;j < curves[i].length;j++){
+
+            const element = curves[i][j];
+            if (isInCircle(element, click)){
+                moveIndexI = i;
+                moveIndexJ = j;
+                touchedAny = true;
+            }
+        }
+    }
+    return touchedAny;
+}
+
+canvas.addEventListener('mousemove', function(e) {
+    if (move) {
+        curves[moveIndexI][moveIndexJ].x = e.offsetX;
+        curves[moveIndexI][moveIndexJ].y = e.offsetY;
+        drawEverything();
+    }
+});
+
+canvas.addEventListener('mouseup', function(e) {
+    move = false;
+});
 
 degree.addEventListener("change", (e) => {
     newDegree = document.getElementById('degree').value;
@@ -44,20 +124,30 @@ function drawPolygonal(i){
 }
 
 canvas.addEventListener('click', (e) => {
+    move = clickedAnyPoint({
+        x: e.offsetX,
+        y: e.offsetY
+    }) 
     count--;
     var p = new Point(e.offsetX, e.offsetY);
     points.push(p);
-    console.log(p);
+    if(controlPointsElem.checked){
+        draw(e);
+    }
     if(count == 0){
         curves.push(points);
         points = [];
         count = +newDegree+1;
-        console.log(curves);
-        drawPolygonal(curves.length-1); 
-        drawCurve(ctx, curves[curves.length-1], newEval);
+        if(controlPoligonalElem.checked){
+            drawPolygonal(curves.length-1); 
+        }
+    
+        if(bezierCurveElem.checked){
+            drawCurve(ctx, curves[curves.length-1], newEval);
+        }
         
     }
-    console.log(curves.length);
+    // drawEverything();
 });
 
 document.getElementById("clear").onclick = function(){
@@ -76,15 +166,24 @@ function resizeToFit() {
     resizeCanvas(width, height);
 }
 resizeToFit();
-canvas.addEventListener("click", draw, false);
+//canvas.addEventListener("click", draw, false);
 
+//draw points from the clicks
 function draw(e) {
     var rect = canvas.getBoundingClientRect();
     posx = e.pageX-rect.left;
     posy = e.pageY-rect.top;
     ctx.beginPath();
     ctx.fillStyle = "red";
-    ctx.arc(posx, posy, 4 , 0, 2*Math.PI);
+    ctx.arc(posx, posy, 8 , 0, 2*Math.PI);
+    ctx.fill();
+}
+
+//draw points from the curves array
+function drawPoints(point){
+    ctx.beginPath();
+    ctx.fillStyle = "red";
+    ctx.arc(point.x, point.y, 8 , 0, 2*Math.PI);
     ctx.fill();
 }
 
@@ -112,11 +211,8 @@ function drawCurve(context, curves, evaluation){
     var t = 0;
     var curvePoints = [];
     for(var i = 0; i < +evaluation+1; i++){
-        console.log("pimba: "+deCasteljau(curves, t));
         curvePoints.push(deCasteljau(curves, t));
-        
         t += 1/evaluation;
-        console.log("t: "+t);
     }
 
     for(var i = 0; i < curvePoints.length-1; i++){
